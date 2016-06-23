c6295b91fcd2d98ef2f282c5afa80bf7493a900d
Daniel Jones <tortxof@gmail.com>
2016-06-23 19:47:08 -0400

    Implement svg for game buttons.

--- src/main.js
+++ src/main.js
@@ -6,26 +6,26 @@ var COLORS = [
 ];
 
 $('.button').on('mousedown', function() {
-  $(this).addClass($(this).data('color') + '-light');
+  $(this).children('path').addClass($(this).data('color') + '-light');
 });
 
 $('.button').on('mouseup', function() {
-  $(this).removeClass($(this).data('color') + '-light');
+  $(this).children('path').removeClass($(this).data('color') + '-light');
 });
 
 function lightOn(color) {
-  $('button.' + color).addClass(color + '-light');
+  $('path.' + color).addClass(color + '-light');
 }
 
 function lightOff(color) {
-  $('button.' + color).removeClass(color + '-light');
+  $('path.' + color).removeClass(color + '-light');
 }
 
 function play_seq(seq) {
   console.log('play_seq');
   for (var i=0; i<seq.length; i++) {
-    setTimeout(lightOn, i * 1000, COLORS[seq[i]]);
-    setTimeout(lightOff, (i * 1000) + 500, COLORS[seq[i]]);
+    setTimeout(lightOn, (i * 1000) + 500 , COLORS[seq[i]]);
+    setTimeout(lightOff, (i * 1000) + 1000, COLORS[seq[i]]);
   }
 }
 
